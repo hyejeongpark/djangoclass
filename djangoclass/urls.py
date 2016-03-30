ac5b386f8d13005c20f0b4dd3558e0338157a3b9
Hyejeong Park <phj1698@gmail.com>
2016-03-30 14:45:50 +0900

    setting medid root

--- djangoclass/urls.py
+++ djangoclass/urls.py
@@ -13,7 +13,9 @@ Including another URLconf
     1. Import the include() function: from django.conf.urls import url, include
     2. Add a URL to urlpatterns:  url(r'^blog/', include('blog.urls'))
 """
+from django.conf import settings
 from django.conf.urls import url
+from django.conf.urls.static import static
 from django.contrib import admin
 
 urlpatterns = [
@@ -23,3 +25,5 @@ urlpatterns = [
     url(r'^hello/(?P<name>[a-zA-Z]+)/$', 'blog.views.hello'),
     url(r'^archives/(?P<year>\d{4})/(?P<month>\d{1,2})/(?P<day>\d{1,2})/', 'blog.views.archives')
 ]
+
+urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
