c34de8f17898942201a746614f871e4297ce353c
Hyejeong Park <phj1698@gmail.com>
2016-04-20 16:19:16 +0900

    Add all url names

--- blog/urls.py
+++ blog/urls.py
@@ -1,7 +1,7 @@
 from django.conf.urls import url
 
 urlpatterns = [
-    url(r'^$', 'blog.views.index'),
+    url(r'^$', 'blog.views.index', name='index'),
     url(r'^(?P<pk>\d+)/$', 'blog.views.post_detail', name='post_detail'),
-    url(r'^new/$', 'blog.views.post_new'),
+    url(r'^new/$', 'blog.views.post_new', name='post_new'),
 ]
