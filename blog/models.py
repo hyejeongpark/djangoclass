f64a26d02eb42e98b5279cc7d6917a1dc42c2d30
Hyejeong Park <phj1698@gmail.com>
2016-04-20 16:39:25 +0900

    Fix RemovedInDjango110Warning

--- blog/models.py
+++ blog/models.py
@@ -5,3 +5,9 @@ class Post(models.Model):
     title = models.CharField(max_length=100)
     content = models.TextField()
     photo = models.ImageField()
+
+    def __str__(self):
+        return self.title
+
+    def get_absolute_url(self):
+        return reverse('blog:detail', args=[self.pk])
--- blog/urls.py
+++ blog/urls.py
@@ -1,7 +1,8 @@
 from django.conf.urls import url
+import blog.views
 
 urlpatterns = [
-    url(r'^$', 'blog.views.index', name='index'),
-    url(r'^(?P<pk>\d+)/$', 'blog.views.post_detail', name='post_detail'),
-    url(r'^new/$', 'blog.views.post_new', name='post_new'),
+    url(r'^$', blog.views.index, name='index'),
+    url(r'^(?P<pk>\d+)/$', blog.views.post_detail, name='post_detail'),
+    url(r'^new/$', blog.views.post_new, name='post_new'),
 ]
