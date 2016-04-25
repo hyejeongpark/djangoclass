96f5efd1c565bd4d04de87197515796edcd1ff68
Hyejeong Park <phj1698@gmail.com>
2016-04-25 21:32:32 +0900

    Make get_absoulute_url

--- blog/views.py
+++ blog/views.py
@@ -16,7 +16,15 @@ def post_new(request):
         form = PostForm(request.POST)
         if form.is_valid():
             post = form.save()
-            return redirect('blog:post_detail', post.pk)
+            # 방법 1)
+            # url = reverse('blog:post_detail', args=[post.pk])
+            # return redirect(url)
+
+            # 방법 2)
+            # return redirect(post.get_absolute_url())
+
+            # 방법 3)
+            return redirect(post)
     else:
         form = PostForm()
     return render(request, 'blog/post_form.html', {'form': form,})
